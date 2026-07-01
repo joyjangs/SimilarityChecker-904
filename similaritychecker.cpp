60d86167d23ab770d6885085aa6e67c6bfa0f1cf
Jongin-Jang <joyjangs07@gmail.com>
2026-07-01 16:08:23 +0900

    [feature] subscore

--- similaritychecker.cpp
+++ similaritychecker.cpp
@@ -7,7 +7,16 @@ class SimilarityChecker {
 public:
 	int getLengthScore(const string& str1, const string& str2) {
 		if (str1.length() == str2.length()) return MAX_LENGTH_SCORE;
-		if (str1.length() >= str2.length() * 2 || str2.length() >= str2.length()) return 0;
-		return 0;
+		if (str1.length() >= str2.length() * 2 || str2.length() >= str1.length()*2) return 0;
+		
+		int result = 0;
+		if (str1.length() > str2.length()) {
+			result = (int) ((1.0 - (double)(str1.length() - str2.length()) / (double)str2.length()) * 60.0);
+		}
+		else {
+			result = (int)((1.0 - (double)(str2.length() - str1.length()) / (double)str1.length()) * 60.0);
+		}
+		
+		return result;
 	}
 };
--- similaritychecker_test.cpp
+++ similaritychecker_test.cpp
@@ -16,9 +16,15 @@ TEST_F(SimilarityFixture, SameLength)
 	checkLengthScore(60, "ASD", "DSA");
 }
 
-TEST_F(SimilarityFixture, SameLength)
+TEST_F(SimilarityFixture, LongerThanTwice)
 {
 	checkLengthScore(0, "A", "BB");
+}
+
+TEST_F(SimilarityFixture, SubScore)
+{
+	checkLengthScore(20, "AAABB", "BAA");
+	checkLengthScore(30, "AA", "AAE");
 }
 
 int main()
