141e707422016ba47a03d8cfd3ffa56af5c941a2
Jongin-Jang <joyjangs07@gmail.com>
2026-07-01 15:04:04 +0900

    초기 커밋.

---
+++ similaritychecker_test.cpp
@@ -0,0 +1,15 @@
+﻿#include "gmock/gmock.h"
+#include "similaritychecker.cpp"
+
+using namespace testing;
+
+TEST(SimilarityCheck, TC1)
+{
+
+}
+
+int main()
+{
+	InitGoogleMock();
+	RUN_ALL_TESTS();
+}
