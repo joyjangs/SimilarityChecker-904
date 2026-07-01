5084ebbffe3cd9a9f86f691a7f7fb32d076a0ac8
Jongin-Jang <joyjangs07@gmail.com>
2026-07-01 15:56:20 +0900

    [refactoring] same length case

--- similaritychecker.cpp
+++ similaritychecker.cpp
@@ -3,9 +3,10 @@
 using std::string;
 
 class SimilarityChecker {
+	const int MAX_LENGTH_SCORE = 60;
 public:
 	int getLengthScore(const string& str1, const string& str2) {
-		if (str1.length() == str2.length()) return 60;
+		if (str1.length() == str2.length()) return MAX_LENGTH_SCORE;
 		return 0;
 	}
 };
